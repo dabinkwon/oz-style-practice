38fadc097fd68204f4df0eeba1eabbd82817c524
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-08-20 14:13:44 +0900

    createGlobalStyle로 전역 CSS 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
 import { flexMixin} from "./styled/styled";
+import { useState } from "react";
+
 
 const StyledSection = styled.section`
   ${flexMixin({justify:"center",align:
     "center",wrap:"wrap",gap:"20px"
   })}
   padding: 20px 40px;
+`
+const GlobalStyle = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: white;
+}
+
+body {
+  background-color: #121212;
+}
+header{
+  background-color: ${(props)=>(props.isLight?"white":"black")};
+  transition: 0.3s ease;
+
+  h1{
+    color: ${(props)=>(props.isLight?"black":"white")};
+  }
+  li{
+    color: ${(props)=>(props.isLight?"black":"white")};
+  }
+}
 
 `
+
 function App() {
-  const ids = contents.map(el=>el.id);
-  console.log(ids)
+  const [isLight,setIsLight] = useState(false)
   return (
     <main>
-      <Header />
-      <StyledSection>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </StyledSection>
-    </main>
+        <GlobalStyle isLight={isLight}/>
+        <Header setIsLight={setIsLight}/>
+        <StyledSection>
+          {contents.map((el) => (
+            <Content key={el.id} content={el} />
+          ))}
+        </StyledSection>
+      </main>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,32 @@ import { flexMixin, fontMixin } from "../styled/styled";
 const StyledHeader = styled.header`
   ${flexMixin({justify:"space-between",align: "center"})}
   padding: 20px 20px;
-  background-color: black;
+
   ul {
     ${flexMixin({justify:"center",align:"center",gap:"20px"})}
     li {
-      ${fontMixin({size:"16px",weight:"400"})}
+      ${fontMixin({size:"16px",weight:"700"})}
       list-style: none;
     }
   }
+  button{
+    color:black;
+    font-weight:600;
+    padding: 2px;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+
 `;
 
-export default function Header() {
+export default function Header({setIsLight}) {
+
   return (
       <StyledHeader>
         <h1>OZ코딩스쿨</h1>
         <ul className="menu-container">
+          <button onClick={()=>setIsLight(prev=>!prev)}>BrightMode</button>
           <li>로그인</li>
           <li>회원가입</li>
           <li>내클래스</li>
